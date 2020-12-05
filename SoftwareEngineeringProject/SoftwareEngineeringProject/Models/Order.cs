9c3758c88d46b6e3584f8edcd80befef22c322d7
GregoryJHicks <34341858+GregoryJHicks@users.noreply.github.com>
2020-12-04 22:26:02 -0500

    Added necessary files
    Removed privacy page, added blank cshtml pages for cart, orders, payment, and receipt. Added blank Menu and Order Models. Found layout page in the project and updated the website's title and nav bar. Also prepped layour for css change.

---
+++ SoftwareEngineeringProject/SoftwareEngineeringProject/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftwareEngineeringProject.Models
+{
+    public class Order
+    {
+        public string thing { get; set; }
+        public int something { get; set; }
+        public string ansdjasdisad { get; set; }
+    }
+}
