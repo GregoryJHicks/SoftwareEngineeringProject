8bade100db0f04179bffffb24f6866f5fce27372
MacaroniBot5 <75452231+MacaroniBot5@users.noreply.github.com>
2020-12-05 22:10:37 -0500

    Revert "Merge branch 'Omega-Branch' into main"
    This reverts commit fefa15a5e07432f7025c0dace746705eb9fe909e, reversing
    changes made to 3b6fda215878c39d1b9c998658e04d3bef419852.

--- SoftwareEngineeringProject/SoftwareEngineeringProject/Models/MenuItem.cs
+++ SoftwareEngineeringProject/SoftwareEngineeringProject/Models/MenuItem.cs
@@ -8,11 +8,11 @@ namespace SoftwareEngineeringProject.Models
     //take in selected items from FullMenu Linked List and turn it into objects that are sent to the cart
     public class MenuItem
     {
-        public int food_id { get; set; }
-        public string food_name { get; set; }
-        public float price { get; set; }
-        public string description { get; set; }
+        public int food_id;
+        public string food_name;
+        public float price;
+        public string description;
 
-        
+
     }
 }
